bb7ea0fb50f14cb787bf7d9a0b3b46497f4ae6a2
daheller <david.heller2019@gmail.com>
2026-03-11 19:52:23 -0400

    Cleaned up AnalyticsService.cs

--- WorkoutTrackerV2/Services/AnalyticsService.cs
+++ WorkoutTrackerV2/Services/AnalyticsService.cs
@@ -2,27 +2,19 @@
 
 namespace WorkoutTrackerV2.Services
 {
-    public class AnalyticsService : IAnalyticsService
+    public class AnalyticsService(IWorkoutService workoutService) : IAnalyticsService
     {
-        private readonly IWorkoutService _workoutService;
-
-        public AnalyticsService(IWorkoutService workoutService)
-        {
-            _workoutService = workoutService;
-        }
-
+        #region "GET DAILY STATS ASYNC"
         public async Task<List<DailyStats>> GetDailyStatsAsync(int days = 30)
         {
-            var startDate = DateTime.Now.AddDays(-days).Date;
-            var endDate = DateTime.Now.Date;
-            var sessions = await _workoutService.GetSessionsAsync(startDate, endDate);
-
             var dailyStatsDict = new Dictionary<DateTime, DailyStats>();
-
+            var startDate = DateTime.Now.AddDays(-days).Date;
+            var endDate = DateTime.Now.Date;           
+            var sessions = await workoutService.GetSessionsAsync(startDate, endDate);
             foreach (var session in sessions)
             {
                 var sessionDate = session.Date.Date;
-                var sets = await _workoutService.GetSetsForSessionAsync(session.Id);
+                var sets = await workoutService.GetSetsForSessionAsync(session.Id);
 
                 if (!dailyStatsDict.ContainsKey(sessionDate))
                 {
@@ -35,15 +27,15 @@ namespace WorkoutTrackerV2.Services
                 stats.TotalRepsCompleted += sets.Sum(s => s.Reps);
                 stats.TotalWeightLifted += sets.Sum(s => s.Weight * s.Reps);
             }
-
             return dailyStatsDict.Values.OrderBy(x => x.Date).ToList();
         }
+        #endregion
 
+        #region "GET EXERCISE PROGRESS ASYNC"
         public async Task<ExerciseProgress> GetExerciseProgressAsync(int exerciseId, int days = 30)
         {
-            var sets = await _workoutService.GetExerciseHistoryAsync(exerciseId, days);
-            var exercise = await _workoutService.GetExerciseAsync(exerciseId);
-
+            var sets = await workoutService.GetExerciseHistoryAsync(exerciseId, days);
+            var exercise = await workoutService.GetExerciseAsync(exerciseId);
             var progress = new ExerciseProgress
             {
                 ExerciseName = exercise.Name,
@@ -52,16 +44,16 @@ namespace WorkoutTrackerV2.Services
                 AverageWeight = sets.Count > 0 ? sets.Average(s => s.Weight) : 0,
                 TotalReps = sets.Sum(s => s.Reps)
             };
-
             return progress;
         }
+        #endregion
 
+        #region "GET PROGRESS FOR MUSCLE GROUP ASYNC"
         public async Task<List<ExerciseProgress>> GetProgressForMuscleGroupAsync(string muscleGroup, int days = 30)
         {
-            var exercises = await _workoutService.GetAllExercisesAsync();
-            var muscleExercises = exercises.Where(e => e.MuscleGroup == muscleGroup).ToList();
-
             var progressList = new List<ExerciseProgress>();
+            var exercises = await workoutService.GetAllExercisesAsync();
+            var muscleExercises = exercises.Where(e => e.MuscleGroup == muscleGroup).ToList();           
             foreach (var exercise in muscleExercises)
             {
                 var progress = await GetExerciseProgressAsync(exercise.Id, days);
@@ -70,15 +62,15 @@ namespace WorkoutTrackerV2.Services
                     progressList.Add(progress);
                 }
             }
-
             return progressList.OrderByDescending(x => x.MaxWeight).ToList();
         }
+        #endregion
 
+        #region "GET STRENGTH PROGRESS ASYNC"
         public async Task<Dictionary<string, double>> GetStrengthProgressAsync(int days = 30)
-        {
-            var exercises = await _workoutService.GetAllExercisesAsync();
+        {  
             var strengthDict = new Dictionary<string, double>();
-
+            var exercises = await workoutService.GetAllExercisesAsync();
             foreach (var exercise in exercises)
             {
                 var progress = await GetExerciseProgressAsync(exercise.Id, days);
@@ -87,15 +79,18 @@ namespace WorkoutTrackerV2.Services
                     strengthDict[exercise.Name] = progress.MaxWeight;
                 }
             }
-
             return strengthDict.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
         }
+        #endregion
 
+        #region "GET CURRENT STREAK"
         public async Task<int> GetCurrentStreak()
         {
-            var allSessions = await _workoutService.GetAllSessionsAsync();
+            var allSessions = await workoutService.GetAllSessionsAsync();
             if (allSessions.Count == 0)
+            {
                 return 0;
+            }             
 
             int streak = 0;
             var today = DateTime.Now.Date;
@@ -106,27 +101,32 @@ namespace WorkoutTrackerV2.Services
                 var hasSessionOnDate = allSessions.Any(s => s.Date.Date == checkDate);
 
                 if (hasSessionOnDate)
+                {
                     streak++;
+                }                  
                 else if (i > 0)
+                {
                     break;
+                }    
             }
-
             return streak;
         }
+        #endregion
 
+        #region "GET AVERAGE WORKOUT DURATION ASYNC"
         public async Task<double> GetAverageWorkoutDurationAsync(int days = 30)
         {
             var startDate = DateTime.Now.AddDays(-days).Date;
             var endDate = DateTime.Now.Date;
-            var sessions = await _workoutService.GetSessionsAsync(startDate, endDate);
+            var sessions = await workoutService.GetSessionsAsync(startDate, endDate);
 
             if (sessions.Count == 0)
+            {
                 return 0;
-
+            }
             return sessions.Average(s => s.Duration.TotalMinutes);
         }
-
-
+        #endregion
 
     }
 }
