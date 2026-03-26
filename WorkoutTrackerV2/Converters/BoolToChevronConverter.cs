28f06f108d166c6b113fbdf3c83b92519024f1ae
daheller <david.heller2019@gmail.com>
2026-03-25 21:08:33 -0400

    Fixed the cheveron

--- WorkoutTrackerV2/Converters/BoolToChevronConverter.cs
+++ WorkoutTrackerV2/Converters/BoolToChevronConverter.cs
@@ -5,7 +5,7 @@ namespace WorkoutTrackerV2.Converters
     public class BoolToChevronConverter : IValueConverter
     {
         public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-            => value is bool b && b ? "▲" : "▼";
+            => value is bool b && b ?  "▼" : "▲";
 
         public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
             => throw new NotImplementedException();
