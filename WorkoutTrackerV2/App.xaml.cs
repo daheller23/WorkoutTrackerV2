e3d363eedee087638adb399d7af43d96d0de1f2a
daheller <david.heller2019@gmail.com>
2026-05-10 15:16:40 -0400

    Added dark mode to the HomeView as a test

--- WorkoutTrackerV2/App.xaml.cs
+++ WorkoutTrackerV2/App.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using Plugin.LocalNotification;
 using WorkoutTrackerV2.Services;
 
+#if ANDROID || IOS
+using CommunityToolkit.Maui.Core.Platform;
+using CommunityToolkit.Maui.Core;
+#endif
+
 namespace WorkoutTrackerV2
 {
     public partial class App : Application
@@ -8,20 +13,41 @@ namespace WorkoutTrackerV2
         public App(ISettingsService settingsService)
         {
             InitializeComponent();
-            Application.Current!.UserAppTheme = settingsService.IsDarkMode
-                ? AppTheme.Dark
-                : AppTheme.Light;
+            bool isDark = settingsService.IsDarkMode;
+            Application.Current!.UserAppTheme = isDark ? AppTheme.Dark : AppTheme.Light;
+            Application.Current.RequestedThemeChanged += (s, a) =>
+            {
+                UpdateStatusBar(a.RequestedTheme == AppTheme.Dark);
+            };
+        }
+
+        private void UpdateStatusBar(bool isDark)
+        {
+            MainThread.BeginInvokeOnMainThread(() =>
+            {
+#if ANDROID || IOS
+                var bgColor = isDark ? Color.FromArgb("#1F1F1F") : Colors.White;
+                var iconStyle = isDark ? StatusBarStyle.LightContent : StatusBarStyle.DarkContent;
+                StatusBar.SetColor(bgColor);
+                StatusBar.SetStyle(iconStyle);
+#endif
+            });
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            var window = new Window(new AppShell());
+            window.Created += (s, e) =>
+            {
+                UpdateStatusBar(Application.Current!.UserAppTheme == AppTheme.Dark);
+            };
+            return window;
         }
+
         protected override async void OnStart()
         {
             base.OnStart();
             await LocalNotificationCenter.Current.RequestNotificationPermission();
         }
-
     }
 }
